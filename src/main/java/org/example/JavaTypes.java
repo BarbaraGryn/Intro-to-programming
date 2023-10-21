2b988ad6fc3d11d01e2e62c8301bf44e5f170102
Barbara Grünberg <barbara.grynberg@gmail.com>
2023-10-21 13:18:17 +0300

    java types int and double - introducing java primitive type

---
+++ src/main/java/org/example/JavaTypes.java
@@ -0,0 +1,30 @@
+package org.example;
+
+public class JavaTypes {
+    // psvm
+    public static void main(String[] args) {
+        // sout
+        System.out.println("Java types example");
+        System.out.println("Numbers first");
+
+        // variable - place for storing some content
+        // has type and value
+        // int - integer number
+        // age - name of variable
+        int age;
+        age = 18;
+        age = 20;
+        System.out.println("Current age is: " + age);
+        age = 25;
+        System.out.println("Current age is: " + age);
+        int age2 = 19;
+        int age3 = 0;
+        System.out.println("Current age is: " + age3);
+
+        // double used for floating numbers
+        double age5 = 4.5;
+        System.out.println("Age5 is: " + age5);
+
+
+    }
+}
