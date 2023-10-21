edf04f4c002b0624b5c0283f8cb186b353ab9f12
Barbara Grünberg <barbara.grynberg@gmail.com>
2023-10-21 11:32:43 +0300

    my first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        // some useful shortcut
+        // line duplication
+        // ctrl + d
+        System.out.println("Hello world!");
+        System.out.println("I am here!");
+        System.out.println("Yes, it's me!");
+    }
+}
