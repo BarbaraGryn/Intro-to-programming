adca178fdfe3f15034defe8f64e57921abc7446d
Barbara Grünberg <barbara.grynberg@gmail.com>
2023-10-21 13:32:23 +0300

    java types long, byte, short

--- src/main/java/org/example/JavaTypes.java
+++ src/main/java/org/example/JavaTypes.java
@@ -25,6 +25,31 @@ public class JavaTypes {
         double age5 = 4.5;
         System.out.println("Age5 is: " + age5);
 
+        // int class = 5;  ei lase teha, sest class on miskit muud
+
+        System.out.println("Let's meet number type");
+        System.out.println("int type");
+        System.out.println("Smallest int is: " + Integer.MIN_VALUE);
+        System.out.println("Biggest int is: " + Integer.MAX_VALUE);
+
+        System.out.println("long type");
+        long longAge = 5;
+        System.out.println("Smallest long is: " + Long.MIN_VALUE);
+        System.out.println("Biggest long is: " + Long.MAX_VALUE);
+
+        System.out.println();
+
+        System.out.println("byte type");
+        byte byteAge = 5;
+        System.out.println("Smallest byte is: " + Byte.MIN_VALUE);
+        System.out.println("Biggest byte is: " + Byte.MAX_VALUE);
+
+        System.out.println();
+
+        System.out.println("short type");
+        short shortAge= 5;
+        System.out.println("Smallest short is: " + Short.MIN_VALUE);
+        System.out.println("Smallest short is: " + Short.MAX_VALUE);
 
     }
 }
