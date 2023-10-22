7593397c9a2418a07dcbb8a96a79aca8448bab1e
Barbara Grünberg <barbara.grynberg@gmail.com>
2023-10-22 12:29:40 +0300

    Very simple calculator

---
+++ src/main/java/org/example/SimpleCalculator.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class SimpleCalculator {
+
+    public static void main(String[] args) {
+
+        System.out.println("Starting calculations");
+
+        addTwoNumbers(1, 2);
+        addTwoNumbers(25, 17);
+        addTwoNumbers(255, 17);
+
+        int first = 5;
+        int second = 6;
+        addTwoNumbers(first, second);
+        addTwoNumbers(first, first);
+
+    }
+
+    public static void addTwoNumbers(int firstNumber, int secondNumber) {
+        System.out.println("First number is: " + firstNumber + ", second number is: "+ secondNumber);
+
+        int sum = firstNumber + secondNumber;
+        System.out.println("Sum of two numbers is: "+ sum);
+
+    }
+}
