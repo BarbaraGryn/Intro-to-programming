f09ed55e9ff9d3bd30a0f915ec8fc9b6ef335451
Barbara Grünberg <barbara.grynberg@gmail.com>
2023-10-28 12:12:22 +0300

    java types - intro to char

--- src/main/java/org/example/JavaTypes.java
+++ src/main/java/org/example/JavaTypes.java
@@ -1,9 +1,8 @@
 package org.example;
 
 public class JavaTypes {
-    // psvm
+
     public static void main(String[] args) {
-        // sout
         System.out.println("Java types example");
         System.out.println("Numbers first");
 
@@ -13,24 +12,30 @@ public class JavaTypes {
         // age - name of variable
         int age;
         age = 18;
+        System.out.println("Current age is: " + age);
+
         age = 20;
         System.out.println("Current age is: " + age);
         age = 25;
         System.out.println("Current age is: " + age);
         int age2 = 19;
-        int age3 = 0;
-        System.out.println("Current age is: " + age3);
+        int age3 = -5;
+        System.out.println("Current age3 is: " + age3);
+
+        int age4 = 4;
 
         // double used for floating numbers
         double age5 = 4.5;
         System.out.println("Age5 is: " + age5);
 
-        // int class = 5;  ei lase teha, sest class on miskit muud
+//        int class = 5;
 
-        System.out.println("Let's meet number type");
+        System.out.println("Let's meet numbers type");
         System.out.println("int type");
         System.out.println("Smallest int is: " + Integer.MIN_VALUE);
         System.out.println("Biggest int is: " + Integer.MAX_VALUE);
+
+        System.out.println();
 
         System.out.println("long type");
         long longAge = 5;
@@ -47,29 +52,31 @@ public class JavaTypes {
         System.out.println();
 
         System.out.println("short type");
-        short shortAge= 5;
+        short shortAge = 5;
         System.out.println("Smallest short is: " + Short.MIN_VALUE);
-        System.out.println("Smallest short is: " + Short.MAX_VALUE);
+        System.out.println("Biggest short is: " + Short.MAX_VALUE);
 
         System.out.println();
 
         System.out.println("double type");
         double doubleAge = 1.5;
         System.out.println("Smallest double is: " + Double.MIN_VALUE);
-        System.out.println("Smallest double is: " + Double.MAX_VALUE);
+        System.out.println("Biggest double is: " + Double.MAX_VALUE);
 
         System.out.println();
 
         System.out.println("float type");
         float floatAge = 1.5f;
         System.out.println("Smallest float is: " + Float.MIN_VALUE);
-        System.out.println("Smallest float is: " + Float.MAX_VALUE);
+        System.out.println("Biggest float is: " + Float.MAX_VALUE);
 
         System.out.println();
 
         System.out.println("char type");
-        char singleLetter = 'a'; // nout "aaa"
-        // TODO: explain range
+        char singleLetter = 'a'; // "ddd"
+        char aLatter = 97; // index of 'a' letter from table
+        System.out.println("A letter: " + singleLetter);
+        System.out.println("A letter: " + aLatter);
 
         System.out.println();
 
